7a58a30a56fd57cedf3e109f45eb40d1437d3c0f
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-19 18:56:26 +0300

    Add task48

---
+++ task48/Program.cs
@@ -0,0 +1,29 @@
+﻿// Create a two-dimensional array m*n. Every element is calculated as m+n. Print the resulting array.
+
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            array[i, j] = i + j;
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Enter the dimension m*n of array: ");
+string[] str = Console.ReadLine().Split(" ");
+
+int m = Convert.ToInt32(str[0]);
+int n = Convert.ToInt32(str[1]);
+
+int[,] arr = CreateArray(m, n);
+PrintArray(arr);
