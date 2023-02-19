6a2a52d0d4012c08475803c2c71af7a0a1c9a07f
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-19 18:52:21 +0300

    Add task49

---
+++ task49/Program.cs
@@ -0,0 +1,42 @@
+﻿// Create a two-dimensional array m*n. Find elements with even indexes and replace them their squares.
+
+int[,] CreateArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+void ReplaceEvenIndex(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 2)
+            array[i, j] *= array[i, j];
+    }
+}
+
+Console.WriteLine("Enter the dimension m*n of array: ");
+string[] str = Console.ReadLine().Split(" ");
+
+int m = Convert.ToInt32(str[0]);
+int n = Convert.ToInt32(str[1]);
+
+int[,] arr = CreateArray(m, n, -9, 9);
+PrintArray(arr);
+ReplaceEvenIndex(arr);
+Console.WriteLine();
+PrintArray(arr);
+
