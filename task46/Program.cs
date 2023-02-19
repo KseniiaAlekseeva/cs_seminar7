a00bdf0f2c07864983d1265dee1b45bb65cd33c5
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-19 18:43:57 +0300

    Add task46

---
+++ task46/Program.cs
@@ -0,0 +1,28 @@
+﻿// Create a two-dimensional array m*n with random integer numbers
+
+int[,] CreateArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Enter the dimension m*n of array: ");
+string[] str = Console.ReadLine().Split(" ");
+
+int m = Convert.ToInt32(str[0]);
+int n = Convert.ToInt32(str[1]);
+int[,] arr = CreateArray(m, n, 0, 9);
+PrintArray(arr);
